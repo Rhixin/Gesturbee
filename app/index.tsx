e6282b44af8112a841c30c210236c14eb0d5849e
Rhixin <vallestedted@gmail.com>
2025-04-06 17:46:32 +0800

    [Zhazted] Added logo in landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 // app/index.tsx (Home Screen)
-import { Text, View, Pressable, Button } from "react-native";
+import { Text, View, Pressable, Image } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function LandingPage() {
@@ -15,14 +15,26 @@ export default function LandingPage() {
 
   return (
     <View className="flex flex-col items-center justify-center h-[100vh] bg-gradient-to-b from-[#00BFAF] to-[#104846]">
-      <Text className="font-poppins-bold text-2xl tracking-widest text-white">
-        GESTUR
-        <Text className="text-secondary tracking-widest">BEE</Text>
-      </Text>
+      <View className="flex flex-row gap-4">
+        <Image
+          source={require("../assets/images/Bee.png")}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+        />
+        <View className="flex justify-center">
+          <Text className="font-poppins-bold text-3xl tracking-widest text-white">
+            GESTUR
+            <Text className="text-secondary tracking-widest">BEE</Text>
+          </Text>
+
+          <Text className="text-white text-sm">
+            Turning Gestures into Conversations
+          </Text>
+        </View>
+      </View>
 
       <Pressable
         onPress={navigateToLogin}
-        className="bg-white px-6 py-2 rounded-lg mx-auto min-w-[200px] mt-4"
+        className="bg-white px-6 py-2 rounded-lg mx-auto min-w-[300px] mt-4"
       >
         <Text className="text-primary font-poppins-bold text-center ">
           Log In
@@ -30,7 +42,7 @@ export default function LandingPage() {
       </Pressable>
       <Pressable
         onPress={navigateToRegister}
-        className="bg-white px-6 py-2 rounded-lg mx-auto min-w-[200px] mt-4"
+        className="bg-white px-6 py-2 rounded-lg mx-auto min-w-[300px] mt-4"
       >
         <Text className="text-primary font-poppins-bold text-center">
           Register
